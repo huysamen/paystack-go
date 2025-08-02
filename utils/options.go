badc06d8f6af7f7f9984a4ca2779b29a9b5a97c1
Nico Huysamen <nico@huysamen.co.za>
2025-08-02 08:47:00 +0200

    enh: add utility functions for functional options pattern

---
+++ utils/options.go
@@ -0,0 +1,38 @@
+// Package options provides utility functions for implementing functional options pattern
+// across the Paystack Go SDK.
+package options
+
+import "time"
+
+// Helper functions for creating pointers to primitive types
+// These are useful for optional fields in request structs
+
+// String returns a pointer to the given string value
+func String(s string) *string {
+	return &s
+}
+
+// Int returns a pointer to the given int value
+func Int(i int) *int {
+	return &i
+}
+
+// Uint64 returns a pointer to the given uint64 value
+func Uint64(u uint64) *uint64 {
+	return &u
+}
+
+// Bool returns a pointer to the given bool value
+func Bool(b bool) *bool {
+	return &b
+}
+
+// Float64 returns a pointer to the given float64 value
+func Float64(f float64) *float64 {
+	return &f
+}
+
+// Time returns a pointer to the given time.Time value
+func Time(t time.Time) *time.Time {
+	return &t
+}
